325aaa0787a9084f3829a449b2cb29258539b4ca
aqzholsky <98736657+aqzholsky@users.noreply.github.com>
2023-09-05 23:11:24 +0600

    addded repeat decorator & refactored

---
+++ bot/utils.py
@@ -0,0 +1,25 @@
+import functools
+import time
+
+
+def repeat_if_failed(count: int, timeout: int = 1, handled_exceptions=None):
+    handled_exceptions = handled_exceptions or (Exception,)
+
+    def inner_function(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            attempt = 0
+            data = {}
+            while attempt < count:
+                attempt += 1
+                try:
+                    data = func(*args, **kwargs)
+                except handled_exceptions:
+                    time.sleep(timeout)
+                else:
+                    break
+            return data
+
+        return wrapper
+
+    return inner_function
